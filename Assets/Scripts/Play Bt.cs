bd0f00e81f8b2a77f2a3b816be73cc28e33cfb70
Ji Hoon Yeo <devhoon1999@gmail.com>
2025-06-02 10:10:19 +0900

    [Feat] 게임 시작 버튼 스크립트 추가
    게임 시작 버튼 누를 시 타운 씬으로 넘어가는 스크립트를 추가함.

---
+++ Assets/Scripts/Play Bt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBt : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.Instance.LoadScene("Town");
+    }
+}
