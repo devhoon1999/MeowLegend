c20d7b81850d45730ac89802385756fde64539c3
Ji Hoon Yeo <devhoon1999@gmail.com>
2025-06-02 22:57:35 +0900

    [Feat] 로딩 씬 수정
    로딩 씬에 배경 이미지를 추가하고, 로딩 바의 디자인을 변경함.

--- Assets/Scripts/Settings Bt.cs
+++ Assets/Scripts/Settings Bt.cs
@@ -7,6 +7,5 @@ public class SettingsBt : MonoBehaviour
     public void SetingsButton()
     {
         PopupManager.Instance.ShowPopup(PopupType.AudioSettings);
-        Debug.Log("버튼 눌림");
     }
 }
